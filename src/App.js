3453462eb404b876afecdf4caf9e061b9cb3f938
siavash <siablack370@gmail.com>
2025-05-26 17:24:06 +0330

    set labels page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import './App.css';
 import {Grid} from "@mui/material";
 
@@ -9,36 +11,49 @@ function App() {
     <div className="App">
       <header className="App-header">
        <div className={'container'}>
-          <div style={{position: 'absolute', zIndex: '1', padding: '1.8rem 0 0 2.5rem'}}>
-              <span style={{fontSize: 44, fontWeight: 800}}>Sunshine</span> <br/>
-              <span style={{fontSize: 12, position: 'absolute', margin: '-10px 0 0 15px'}}>CRAFT BEER</span>
-          </div>
-          <Grid container>
-             <Grid style={{ height: '100vh'}} size={6}>
-                 <video
-                     src="/beer-video.mp4"
-                     autoPlay
-                     loop
-                     muted
-                     playsInline
-                     width={1000}
-                     style={{
-                         position: 'absolute',
-                         top: 0,
-                         left: 0,
-                         width: '50%',
-                         height: '100%',
-                         objectFit: 'cover',
-                     }}
-                 />
-             </Grid>
-             <Grid style={{}} size={6}> 2</Grid>
-          </Grid>
-          <div style={{position: 'absolute', zIndex: '1', bottom: '2.5rem', left: '2.5rem', display: 'flex', gap: 10}}>
-            <InstagramIcon fontSize={'large'}/>
-            <LinkedInIcon fontSize={'large'}/>
-            <GitHubIcon fontSize={'large'}/>
-          </div>
+           <div className={'labels'}>
+               <span style={{fontSize: 44, zIndex: '1',  position: 'absolute', left: '2.5rem', fontWeight: 800}}>Sunshine</span> <br/>
+               <span style={{fontSize: 12, zIndex: '1',  position: 'absolute', left: '9rem', top: '3rem'}}>CRAFT BEER</span>
+               <span style={{fontSize: 14, zIndex: '1', position: 'absolute', right: '2.5rem', top: '1.5rem', color: 'gray', fontWeight: 800}}>MADE WITH</span> <br/>
+               <span style={{fontSize: 25, zIndex: '1', position: 'absolute', right: '2.5rem', top: '2.3rem'}}>Siavash Mir</span>
+               <div style={{position: 'absolute', zIndex: '1', bottom: '2.5rem', left: '2.5rem', display: 'flex', gap: 10}}>
+                   <InstagramIcon fontSize={'large'}/>
+                   <LinkedInIcon fontSize={'large'}/>
+                   <GitHubIcon fontSize={'large'}/>
+               </div>
+               <div style={{position: 'absolute', zIndex: '1', bottom: '2.5rem', right: '2.5rem'}}>
+                   <KeyboardArrowUpIcon fontSize={'large'}/>
+                   <KeyboardArrowDownIcon fontSize={'large'}/>
+               </div>
+           </div>
+           <Grid container spacing={1}>
+
+               <Grid size={6}>
+                   <video
+                       src="/beer-video.mp4"
+                       autoPlay
+                       loop
+                       muted
+                       playsInline
+                       // width={1000}
+                       style={{
+                           position: 'absolute',
+                           top: 0,
+                           left: 0,
+                           width: '50%',
+                           height: '100%',
+                           objectFit: 'cover',
+                       }}
+                   />
+
+               </Grid>
+               <Grid style={{border: '2px solid red'}} size={6}>
+                   <div style={{margin: 'auto'}}>sdfdsfdsssssssssssss</div>
+               </Grid>
+           </Grid>
+
+
+
        </div>
       </header>
     </div>
